c77945e6eeda04bc0c337536cc73f87d4c110ca0
Uchiko <memememomo@gmail.com>
2024-05-04 18:09:28 +0900

    不要コードを削除

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,22 +1,9 @@
 import { StackContext, Api, EventBus, StaticSite } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
-  const bus = new EventBus(stack, "bus", {
-    defaults: {
-      retries: 10,
-    },
-  });
-
   const api = new Api(stack, "api", {
-    defaults: {
-      function: {
-        bind: [bus],
-      },
-    },
     routes: {
       "GET /": "packages/functions/src/lambda.handler",
-      "GET /todo": "packages/functions/src/todo.list",
-      "POST /todo": "packages/functions/src/todo.create",
     },
   });
 
@@ -28,10 +15,6 @@ export function API({ stack }: StackContext) {
       VITE_APP_API_ENDPOINT: api.url,
     }
   })
-
-  bus.subscribe("todo.created", {
-    handler: "packages/functions/src/events/todo-created.handler",
-  });
 
   stack.addOutputs({
     ApiEndpoint: api.url,
